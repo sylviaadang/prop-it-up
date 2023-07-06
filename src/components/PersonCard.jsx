c7178008ccca88ac81319e982d6a88ec8ddc85a4
Your Name <email@example.com>
2023-07-06 12:52:07 -0700

    finished

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,13 +1,22 @@
 import react from 'react';
+import React, {useState} from 'react';
 
 const PersonCard = (props) => {
 
 
+
+    const [click, setClick] = useState(parseInt(props.age))
+
+    const buttonClick = () => {
+        setClick(click+1)
+    }
+
     return (
         <div>
             <h2>{props.name}</h2>
-            <h4>Age: {props.age}</h4>
+            <h4>Age: {click}</h4>
             <h4>Hair Color: {props.haircolor}</h4>
+            <button onClick={ buttonClick }>Birthday Button for {props.name}</button>
         </div>
 
     )
